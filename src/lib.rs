28ee5c9d47cf872def37b8909d0c038c21f13872
Polarit <jjlammi at yahoo>
2019-06-17 17:47:17 +0300

    more functions to library

--- src/lib.rs
+++ src/lib.rs
@@ -26,5 +26,25 @@ pub mod solar {
     }
 
   }
+
+pub fn date_jdn(year: i32, month: i32, day: i32) -> i32 {
+    let jdn2: i32 = (1461 * (year + 4800 + (month - 14) / 12)) / 4
+        + (367 * (month - 2 - 12 * ((month - 14) / 12))) / 12 
+        - (3 * ((year + 4900 + (month - 14) / 12) / 100)) / 4
+        + day
+        - 32075;
+    jdn2 // return value
+}
+
+pub fn jd_epoch(jdn: f64, h: i32, mns: i32) -> f64 {
+    let jd = utc_time_jd(jdn, h, mns);
+    (jd - 2_451_545.0) / 36525.0
+  }
+
+pub fn utc_time_jd(jdn: f64, h: i32, mns: i32) -> f64 {
+    let hdelta: f64 = (f64::from(h) + f64::from(mns) / 60.0) / 24.0;
+    let jd2: f64 = hdelta + jdn - 0.5; 
+    jd2 // return value of function
+  }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use suncalc::solar::trigonos::acosd;
 use suncalc::solar::trigonos::sind; 
 use suncalc::solar::trigonos::asind; 
 use suncalc::solar::trigonos::tand;
+use suncalc::solar::date_jdn;
+use suncalc::solar::jd_epoch;
+use suncalc::solar::utc_time_jd;
 
 const ZENITH_SUNRISE: f64 = 90.83;
 
@@ -24,14 +27,12 @@ fn main() {
     jd_output(my_jdn, 10, 22);
 
     let epoch = jd_epoch(my_jdn, 10, 22);
-    //let sun_mean_anom = sun_mean_anomal(epoch);
     let sun_eq_cntr = sun_equat_centr(epoch); // expected 0.874
     let true_long = sun_true_longit(epoch, sun_eq_cntr); // expected 76.413
     let sun_long = sun_app_long(true_long, epoch);
     let my_declination = declination(23.4359, sun_long); // except 22.74
     let ha_rise = sunrise_ha(latitude, my_declination);
     println!("Epoch 2000 = {:.6}", epoch);
-    // println!("Sun mean anomality     = {:.3} °", sun_mean_anom); // expected 152.27
     println!("Sun equat. centre      =   {:.3} °", sun_eq_cntr); // expected 0.874
     println!("Sun true longitude     =  {:.3} °", true_long);
     println!("Sun apparent longitude =  {:.3} °", sun_long);
@@ -43,36 +44,11 @@ fn sunrise_ha(latit: f64, declinat: f64) -> f64 {
        (acosd(cosd(ZENITH_SUNRISE)/((cosd(latit))*cosd(declinat)) - tand(latit)*tand(declinat)))
 }
 
-fn jd_epoch(jdn: f64, h: i32, mns: i32) -> f64 {
-    let jd = utc_time_jd(jdn, h, mns);
-    (jd - 2_451_545.0) / 36525.0
-}
-
-fn date_jdn(year: i32, month: i32, day: i32) -> i32 {
-    let jdn2: i32 = (1461 * (year + 4800 + (month - 14) / 12)) / 4
-        + (367 * (month - 2 - 12 * ((month - 14) / 12))) / 12
-        - (3 * ((year + 4900 + (month - 14) / 12) / 100)) / 4
-        + day
-        - 32075;
-    jdn2 // return value
-}
-
-fn utc_time_jd(jdn: f64, h: i32, mns: i32) -> f64 {
-    let hdelta: f64 = (f64::from(h) + f64::from(mns) / 60.0) / 24.0;
-    let jd2: f64 = hdelta + jdn - 0.5;
-    jd2 // return value of function
-}
-
 fn jd_output(jdn: f64, h: i32, mn: i32) {
     println!("UTC time: {}h {}min", h, mn);
     let x = utc_time_jd(jdn, h, mn);
     println!("JD = {:.4}", x);
 }
-
-//fn sun_mean_anomal(epoc: f64) -> f64 {
-//    let y: f64 = (357.52911 + epoc * (35999.05029 - 1.537E-4 * epoc)) % 360.0;
-//    y // test return
-//}
 
 fn sun_true_longit(epoc: f64, sun_eq_ctr: f64) -> f64 {
    sun_eq_ctr + (280.46646 + epoc * (36000.76983 + epoc * 3.032E-4)) % 360.0
