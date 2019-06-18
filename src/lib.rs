6b39c537c295d6e54f0260d3db0dfec996bf6949
Polarit <jjlammi at yahoo>
2019-06-18 09:58:54 +0300

    sind cannot be moved

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-pub mod solar {
+  pub mod solar {
+    pub fn sun_true_longit(epoc: f64, sun_eq_ctr: f64) -> f64 {
+        sun_eq_ctr + (280.46646 + epoc * (36000.76983 + epoc * 3.032E-4)) % 360.0
+    }
+  }
+
   pub mod trigonos {
 
     pub fn sind(x: f64) -> f64 {
@@ -27,6 +32,7 @@ pub mod solar {
 
   }
 
+pub mod julian {
 pub fn date_jdn(year: i32, month: i32, day: i32) -> i32 {
     let jdn2: i32 = (1461 * (year + 4800 + (month - 14) / 12)) / 4
         + (367 * (month - 2 - 12 * ((month - 14) / 12))) / 12 
@@ -46,5 +52,5 @@ pub fn utc_time_jd(jdn: f64, h: i32, mns: i32) -> f64 {
     let jd2: f64 = hdelta + jdn - 0.5; 
     jd2 // return value of function
   }
-}
+ }
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::env;    
 use chrono::{Local};
-use suncalc::solar::trigonos::cosd;
-use suncalc::solar::trigonos::acosd;
-use suncalc::solar::trigonos::sind; 
-use suncalc::solar::trigonos::asind; 
-use suncalc::solar::trigonos::tand;
-use suncalc::solar::date_jdn;
-use suncalc::solar::jd_epoch;
-use suncalc::solar::utc_time_jd;
+use suncalc::trigonos::cosd;
+use suncalc::trigonos::acosd;
+use suncalc::trigonos::sind; 
+use suncalc::trigonos::asind; 
+use suncalc::trigonos::tand;
+use suncalc::julian::date_jdn;
+use suncalc::julian::jd_epoch;
+use suncalc::julian::utc_time_jd;
+use suncalc::solar::sun_true_longit;
 
 const ZENITH_SUNRISE: f64 = 90.83;
 
@@ -50,14 +51,6 @@ fn jd_output(jdn: f64, h: i32, mn: i32) {
     println!("JD = {:.4}", x);
 }
 
-fn sun_true_longit(epoc: f64, sun_eq_ctr: f64) -> f64 {
-   sun_eq_ctr + (280.46646 + epoc * (36000.76983 + epoc * 3.032E-4)) % 360.0
-}
-
-fn sun_app_long(sun_true_long: f64, epoch: f64) -> f64 {
-    sun_true_long - 0.00569 - 0.00478 * sind(125.04 - 1934.136 * epoch)
-}
-
 fn sun_equat_centr(epoc: f64) -> f64 {
     let mean_anom: f64 = (357.52911 + epoc * (35999.05029 - 1.537E-4 * epoc)) % 360.0; 
     let arvo: f64 = sind(mean_anom) * (1.914_602 - epoc * (4.817E-3 + 1.4E-5 * epoc))
@@ -65,6 +58,11 @@ fn sun_equat_centr(epoc: f64) -> f64 {
         + sind(3.0 * mean_anom) * 2.89E-4;
     arvo // returned value
 }
+
+fn sun_app_long(sun_true_long: f64, epoch: f64) -> f64 {
+        sun_true_long - 0.00569 - 0.00478 * sind(125.04 - 1934.136 * epoch)
+    } 
+
 fn declination(obl_cor: f64, sun_app_long: f64) -> f64 {
     asind(sind(obl_cor) * sind(sun_app_long))
 }
