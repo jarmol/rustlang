8230f92a68e38ef19416790f7c958acd0eb6b543
Polarit <jjlammi@yahoo.com>
2019-06-21 01:11:54 +0300

    new Sunrise time

--- src/main.rs
+++ src/main.rs
@@ -28,16 +28,30 @@ fn main() {
     let sun_long = sun_app_long(epoch);
     let my_declination = declination(23.4359, sun_long); // except 22.74
     let ha_rise = sunrise_ha(latitude, my_declination);
+    let daylen_tuple = get_hrmn(ha_rise/180.0);
     let noon_fraction = noon_time(epoch, longitude, time_zone);
-    let noon_hours: u32 = (24.0*noon_fraction) as u32;
-    let noon_minutes : u32 = (1440.0*noon_fraction % 60.0) as u32;
+    let rise_fraction = rise_time(noon_fraction, ha_rise);
+    let time_tuple   = get_hrmn(noon_fraction);
+    let rise_tuple   = get_hrmn(rise_fraction);
+   
+    fn get_hrmn(dayfract: f64) -> (u32, u32) {
+       let day_hours:   u32 = (24.0*dayfract) as u32;
+       let day_minutes: u32 = (1440.0*dayfract % 60.0) as u32;
+       (day_hours, day_minutes)
+    }
+
+    fn rise_time(noon_time: f64, ha_angle: f64) -> f64 {
+       noon_time - ha_angle/360.0
+    }
 
     println!("Epoch 2000 = {:.6}", epoch);
     println!("Declination            =  {:.3} °", my_declination);
     println!("HA Sunrise             = {:.3} °", ha_rise); // expect 166.75 deg
     println!("Time equation          = {:.3} min", time_equation(epoch)); // expect -0.858 minutes
-    println!("Day length             =  {:.0} h {:.0} min", 2.0*ha_rise/15.0, 8.0*ha_rise % 60.0);
-    println!("Noon time              = {} h {} min", noon_hours, noon_minutes);
+//  println!("Day length             =  {:.0} h {:.0} min", 2.0*ha_rise/15.0, 8.0*ha_rise % 60.0);
+    println!("Day length             = {} h {} min", daylen_tuple.0, daylen_tuple.1);
+    println!("Sunrise time           = {} h {} min", rise_tuple.0, rise_tuple.1);
+    println!("Noon time              = {} h {} min", time_tuple.0, time_tuple.1);
 
 fn jd_output(jdn: f64, h: i32, mn: i32) {
     println!("UTC time: {}h {}min", h, mn);
