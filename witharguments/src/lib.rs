11423146fd316b773c454148602ed1e839c6f787
Polarit <jjlammi@yahoo.com>
2019-06-30 11:02:57 +0300

    time arguments hr mn ss

---
+++ witharguments/src/lib.rs
@@ -0,0 +1,146 @@
+  pub mod solar {
+  use crate::trigonos::sind;
+  use crate::trigonos::tand;
+  use crate::trigonos::cosd;
+  use crate::trigonos::acosd;
+  use crate::trigonos::asind;
+  
+  const ZENITH_SUNRISE: f64 = 90.83;
+
+  pub fn sun_true_longit(epoc: f64) -> f64 {
+       let sun_eq_cntr = sun_equat_centr(epoc); // expected 0.874 
+       sun_eq_cntr + (280.46646 + epoc * (36000.76983 + epoc * 3.032E-4)) % 360.0
+  }
+
+  pub fn sun_app_long(epoch: f64) -> f64 {
+         let sun_true_long = sun_true_longit(epoch);
+         sun_true_long - 0.00569 - 0.00478 * sind(125.04 - 1934.136 * epoch)
+    } 
+
+  pub fn sun_equat_centr(epoc: f64) -> f64 {
+    //  let sind = |x: f64|  x.to_radians().sin();
+      let mean_anom: f64 = (357.52911 + epoc * (35999.05029 - 1.537E-4 * epoc)) % 360.0; 
+      let arvo: f64 = sind(mean_anom) * (1.914_602 - epoc * (4.817E-3 + 1.4E-5 * epoc))
+        + sind(2.0 * mean_anom) * (0.019_993 - 1.01E-4 * epoc)
+        + sind(3.0 * mean_anom) * 2.89E-4;
+    arvo // returned value
+  }
+
+  pub fn declination(obl_cor: f64, sun_app_long: f64) -> f64 {
+      asind(sind(obl_cor) * sind(sun_app_long))
+  }
+
+  pub fn time_equation(epoc: f64) -> f64 {
+      let mean_long_sun: f64 = (280.46646 + epoc * (36000.76983 + epoc * 3.032E-4)) % 360.0; 
+      let mean_anom: f64 = (357.52911 + epoc * (35999.05029 - 1.537E-4 * epoc)) % 360.0;
+      let eccent_orbit = 0.0167; 
+      let y_var = 0.043;
+      let pal_arvo: f64 = 4.0*(y_var*sind(2.0*(mean_long_sun))
+      - 2.0*eccent_orbit
+      *sind(mean_anom)
+      + 4.0*eccent_orbit*y_var
+      *sind(mean_anom)
+      *cosd(2.0*mean_long_sun)
+      - 0.5*y_var*y_var
+      *sind(4.0*mean_long_sun)
+      - 1.25*eccent_orbit*eccent_orbit
+      *sind(2.0*mean_anom)).to_degrees();
+
+      pal_arvo
+  }
+
+   pub fn sunrise_ha(latit: f64, declinat: f64) -> f64 {
+      (acosd(cosd(ZENITH_SUNRISE)/(cosd(latit)*cosd(declinat)) - tand(latit)*tand(declinat)))
+   }
+
+   pub fn noon_time(epoc: f64, longit: f64, tz: f64) -> f64 {
+       let eq_of_time = time_equation(epoc);
+      (720.0 - 4.0*longit - eq_of_time + tz*60.0)/1440.0 // noontime[minutes]/24[hours]
+   }
+
+   pub fn true_solar_time(hh: u32, mm: u32, ss: u32, longit: f64, epoc: f64, tz: f64) -> f64 {
+    // =MOD(F159*1440+W159+4*$B$4-60*$B$5;1440)
+       let f = hh as f64 / 24.0 + mm as f64 / 1440.0 + ss as f64 / 86400.0;
+       let time_equat: f64 = time_equation(epoc);   // except 1.183;
+   //  let tz = 2.0;
+       (f*1440.0 + time_equat + 4.0*longit - 60.0*tz) % 1440.0
+   }
+
+   pub fn hour_angle(true_sol_time: f64) -> f64 {
+       let ha = if true_sol_time < 0.0 {true_sol_time/4.0 + 180.0}
+       else {true_sol_time/4.0 - 180.0};
+       ha
+    }
+
+   pub fn  solar_zenith_angle(latit: f64, sun_declin: f64, hr_angle: f64) -> f64 {
+       acosd(sind(latit)*sind(sun_declin)+cosd(latit)*cosd(sun_declin)*cosd(hr_angle))    // 43.108 degrees expected
+    } 
+
+   pub fn atmospheric_refraction(h: f64) -> f64 {
+       let x = h.to_radians().tan();
+       let mut refract: f64 = 0.0;
+       if h > 85.0 {refract = 0.0;}
+       else if h > 5.0 {refract = 58.1/x - 0.07/x.powf(3.0) + 0.000086/x.powf(5.0);}
+       else if h > -0.575 {refract = 1735.0 + h*(-518.2 + h*(103.4 + h*(-12.79 + h*0.711)));}
+       else if h < -0.575 {refract = -20.772 / x; };
+
+       refract/3600.0   // returned value
+   }
+
+   pub fn refr_correct_altitude(zenith: f64, height: f64) -> f64 {
+       let refract = atmospheric_refraction(height);
+       90.0 - zenith + refract
+   }
+
+ }
+
+  pub mod trigonos {
+
+    pub fn sind(x: f64) -> f64 {
+        x.to_radians().sin()
+    }
+
+    pub fn asind(x: f64) -> f64 {
+        x.asin().to_degrees()
+    }
+
+    pub fn cosd(x: f64) -> f64 {
+        x.to_radians().cos()
+    }
+
+    pub fn acosd(x: f64) -> f64 {
+        x.acos().to_degrees()
+    }
+
+    pub fn tand(x: f64) -> f64 {
+        x.to_radians().tan()
+    }
+
+    pub fn atand(x: f64) -> f64 {
+        x.atan().to_degrees()
+    }
+
+  }
+
+pub mod julian {
+pub fn date_jdn(year: i32, month: i32, day: i32) -> i32 {
+    let jdn2: i32 = (1461 * (year + 4800 + (month - 14) / 12)) / 4
+        + (367 * (month - 2 - 12 * ((month - 14) / 12))) / 12 
+        - (3 * ((year + 4900 + (month - 14) / 12) / 100)) / 4
+        + day
+        - 32075;
+    jdn2 // return value
+}
+
+pub fn jd_epoch(jdn: f64, h: i32, mns: i32) -> f64 {
+    let jd = utc_time_jd(jdn, h, mns);
+    (jd - 2_451_545.0) / 36525.0
+  }
+
+pub fn utc_time_jd(jdn: f64, h: i32, mns: i32) -> f64 {
+    let hdelta: f64 = (f64::from(h) + f64::from(mns) / 60.0) / 24.0;
+    let jd2: f64 = hdelta + jdn - 0.5; 
+    jd2 // return value of function
+  }
+ }
+
---
+++ witharguments/src/main.rs
@@ -0,0 +1,101 @@
+use std::env;    
+use chrono::{NaiveDate, NaiveDateTime, NaiveTime, Local};
+use suncalcargs::julian::date_jdn;
+use suncalcargs::julian::jd_epoch;
+use suncalcargs::julian::utc_time_jd;
+use suncalcargs::solar::sun_app_long;
+use suncalcargs::solar::sunrise_ha;
+use suncalcargs::solar::declination;
+use suncalcargs::solar::noon_time;
+use suncalcargs::solar::true_solar_time;
+use suncalcargs::solar::hour_angle;
+use suncalcargs::solar::solar_zenith_angle;
+use suncalcargs::solar::atmospheric_refraction;
+use suncalcargs::solar::refr_correct_altitude;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("Program:    {}", args[0]);
+    println!("Read {} arguments", args.len() - 1);
+    let mut v1:u32 = 12;
+    let mut v2:u32 = 22;
+
+// Handle arguments hr, mn, ss
+    if args.len() > 2 {
+       println!("Argument 1: {} hr", args[1]);
+       println!("Argument 2: {} mn", args[2]);
+       v1 = args[1].parse::<u32>().unwrap();
+       v2 = args[2].parse::<u32>().unwrap();
+   }
+
+    let (latitude, longitude, time_zone) = (65.85, 24.18, 2.0);
+    println!("Location: latitude {} °, longitude {} °, time zone {} h", latitude, longitude, time_zone);
+    let local_time = Local::now();
+
+    let mut v3:u32 = 7; // day
+    if v1 < time_zone as u32 {
+       v1 += 24;
+       v3 -= 1;
+    }
+
+    let (year, month, day) = (2019, 6, v3);
+    let (hr, mn, ss) = (v1, v2, 00);
+    let date_time: NaiveDateTime = NaiveDate::from_ymd(year, month, day).and_hms(hr, mn, ss);
+    let my_jdn = f64::from(date_jdn(year, month as i32, day as i32));
+    println!("Time now: {}", local_time.to_rfc2822());
+    println!("Calculation date and time is {}.", date_time);
+
+    println!("JDN = {}", my_jdn);
+    let hr_utc = hr - time_zone as u32;
+    jd_output(my_jdn, hr_utc as i32, mn as i32);
+
+    let epoch = jd_epoch(my_jdn, hr_utc as i32, 22);
+    let sun_long = sun_app_long(epoch);
+    let my_declination = declination(23.4359, sun_long); // except 22.74
+    let ha_rise       =  sunrise_ha(latitude, my_declination);
+    let daylen        =  get_hrmn(ha_rise/180.0);
+    let noon_fraction =  noon_time(epoch, longitude, time_zone);
+    let noon_time     =  get_hrmn(noon_fraction);
+    let rise_fraction =  rise_set_time(noon_fraction, ha_rise);
+    let rise_time     =  get_hrmn(rise_fraction);
+    let set_fraction  =  rise_set_time(noon_fraction, -ha_rise);
+    let set_time      =  get_hrmn(set_fraction);
+    let true_sol_time =  true_solar_time(hr, mn, ss, longitude, epoch, time_zone);
+    let hr_angle = hour_angle(true_sol_time);
+    let sun_zenith = solar_zenith_angle(latitude, my_declination, hr_angle);
+    let sun_max_altitude: f64 = 90.0 - sun_zenith;
+    let atmosfer_refract = atmospheric_refraction(sun_max_altitude); 
+    let correct_height   = refr_correct_altitude(sun_zenith, sun_max_altitude);
+
+ // println!("Epoch 2000                     =  {:.6}", epoch);
+    println!("Declination                    =  {:.3} °", my_declination);
+ // println!("HA Sunrise                     = {:.3} °", ha_rise); // expect 166.75 deg
+    println!("True solar time                = {:.3} min", true_sol_time); // except 720
+    println!("Hour angle                     =  {:.3} °", hr_angle);  // except 0.000
+    println!("Solar zenith                   = {:.3} °", sun_zenith); // expected 43.11 deg
+    println!("Sun altitude                   = {:.3} °", sun_max_altitude); // expected 46.892 deg
+    println!("Atmospheric refraction         =  {:.3} °", atmosfer_refract); // expected 0.015 deg
+    println!("Refraction corrected elevation = {:.3} °", correct_height); // expected 46.91 deg
+    println!("Day length             = {:.?}", daylen);
+    println!("Sunrise time           = {:.?} ", rise_time);
+    println!("Noon time              = {:.?}", noon_time);
+    println!("Sunset time            = {:.?}", set_time);
+  } // End of main
+
+    fn get_hrmn(dayfract: f64) -> NaiveTime {
+       let day_hours:   u32 = (   24.0*dayfract       ) as u32;
+       let day_minutes: u32 = ( 1440.0*dayfract % 60.0) as u32;
+       let day_seconds: u32 = (86400.0*dayfract % 60.0) as u32;
+       NaiveTime::from_hms(day_hours, day_minutes, day_seconds)
+    }
+
+    fn rise_set_time(noon_time: f64, ha_angle: f64) -> f64 {
+       noon_time - ha_angle/360.0
+    }
+
+  fn jd_output(jdn: f64, h: i32, mn: i32) {
+     println!("UTC time: {}h {}min", h, mn);
+     let x = utc_time_jd(jdn, h, mn);
+     println!("JD = {:.4}", x);
+  }
+
