4e3775557a8ec6eba4c4f5582849edc1c1c15d9b
Polarit <jjlammi@yahoo.com>
2019-07-23 12:17:25 +0300

    batch file run

---
+++ witharguments/sunbatch.sh
@@ -0,0 +1,13 @@
+
+echo 1. Helsinki 2019-07-23 DLST UTC + 3 h
+#    1. Helsinki 2019-07-23 DLST UTC + 3 h
+cargo run 13 27 0 2019 7 23 60.16 24.96 3.0
+echo =====================================
+echo 2. Oulu 2019-07-23 DLST UTC + 3 h
+#    2. Oulu 2019-07-23 DLST UTC + 3 h
+cargo run 13 27 0 2019 7 23 65.02 25.5  3.0
+echo =====================================
+echo 3. Stockholm 2019-07-23 DLST UTC + 2 h
+#    3. Stockholm 2019-07-23 DLST UTC + 2 h
+cargo run 12 54 12 2019 7 23 59.33 18.07 2.0
+
